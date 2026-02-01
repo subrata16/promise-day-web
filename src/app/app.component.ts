c55f780aa7c7ecaaf5fb81821fa3feee3a9d1d3c
Subrata Mitra <subratamit35@gmail.com>
2026-02-02 01:32:29 +0530

    Fix for error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'promise-day-web';
+export class AppComponent implements OnInit {
+  hearts: any[] = [];
+
+  ngOnInit() {
+    // Generate 50 hearts with random positions and timings
+    for (let i = 0; i < 50; i++) {
+      this.hearts.push({
+        left: Math.random() * 100,
+        delay: Math.random() * 5,
+        duration: 3 + Math.random() * 5
+      });
+    }
+  }
 }
